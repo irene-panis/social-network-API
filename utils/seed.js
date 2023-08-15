6eb2954bb92c1ecf9c010e11c136808e1eb5c186
irene <panisirene@gmail.com>
2023-08-14 23:15:13 -0700

    add func to mass delete thoughts

--- utils/seed.js
+++ utils/seed.js
@@ -20,6 +20,11 @@ connection.once('open', async () => {
     await connection.dropCollection('users');
   }
 
+  let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+  if (thoughtCheck.length) {
+    await connection.dropCollection('thoughts');
+  }
+
   await User.collection.insertMany(users);
 
   console.table(users);
