9d80411e67496b8d19fb9568918f77f232ba9e13
irene <panisirene@gmail.com>
2023-08-14 23:43:00 -0700

    implement moment.js for date formatting

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
 
 const reactionSchema = Schema(
   {
@@ -19,47 +20,48 @@ const reactionSchema = Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      get: (date) => moment(date).format('MMMM Do YYYY, h:mm:ss a'),
     }
   },
   {
-  _id: false,
+    toJSON: {
+      getters: true,
+    },
+    id: false,
   }
 );
 
-const thoughtSchema = Schema({
-  thoughtText: {
-    type: String,
-    required: true, 
-    minLength: 1,
-    maxLength: 280,
+const thoughtSchema = Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => moment(date).format('MMMM Do YYYY, h:mm:ss a'),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [reactionSchema]
-});
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 thoughtSchema
   .virtual('reactionCount')
   .get(function() {
     return this.reactions.length;
-  });
-
-thoughtSchema
-  .virtual('formatThoughtDate')
-  .get(function() {
-    return this.createdAt.toLocaleString();
-  });
-
-reactionSchema
-  .virtual('formatReactionDate')
-  .get(function() {
-    return this.createdAt.toLocaleString();
   });
 
 const Thought = model('Thought', thoughtSchema);
