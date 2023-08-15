c874c121bbac43455352c026d77cdceb6f414788
irene <panisirene@gmail.com>
2023-08-14 18:39:45 -0700

    create User model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: function(val) {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return emailRegex.test(val);
+      },
+      message: props => `${props.value} is not a valid email address!`,
+    }
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+});
+
+userSchema
+  .virtual('friendCount')
+  .get(function() {
+    return this.friends.length;
+  });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
